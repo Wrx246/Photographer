1a341917a5764e22422b676719c1f4e3813b4dee
Maxim Kuzmianok <jst.wrx@gmail.com>
2022-05-14 01:09:15 +0300

    feat: add pagination

--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -1,5 +1,7 @@
-import { Button } from '@mui/material';
-import { FC, useEffect } from 'react';
+import { Button, Pagination, PaginationItem, Stack } from '@mui/material';
+import { Box } from '@mui/system';
+import { FC, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
@@ -8,16 +10,16 @@ import { IPhoto } from '../../types/photosTypes';
 import st from './Photos.module.scss';
 
 
-
-
 const Photos: FC = () => {
-  const {photos, error, loading} = useTypedSelector(state => state.photos)
-  const {fetchPhotos} = useActions();
+  const { photos, error, loading } = useTypedSelector(state => state.photos)
+  const { fetchPhotos } = useActions();
   const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+  const [totalPage] = useState(13134)
 
-  useEffect(()=> {
-    fetchPhotos(1, 20);
-  },[])
+  useEffect(() => {
+    fetchPhotos(page, 20);
+  }, [page])
 
   return (
     <MainLayout>
@@ -28,23 +30,42 @@ const Photos: FC = () => {
               <div className={st.image}>
                 <img src={photo.urls.raw} alt='alt' />
               </div>
-              {/* <div className={st.info}>
-                <p className={st.title}>Autor: {photo.user?.name}</p>
-                <p className={st.title}>Location: {photo.user?.location ? photo.user?.location : 'Unknown'}</p>
-                <p className={st.title}>Likes: {photo.likes ? photo.likes : '0'}</p>
-              </div> */}
             </div>
             <div className={st.back}>
               <span className={st.desc}>Autor: {photo.user?.name}</span>
               <span className={st.desc}>Location: {photo.user?.location ? photo.user?.location : 'Unknown'}</span>
               <span className={st.desc}>Likes: {photo.likes ? photo.likes : '0'}</span>
-              <Button onClick={()=> navigate(`/photos/${photo.id}`) } variant='contained' color='primary'>Details</Button>
+              <Button onClick={() => navigate(`/photos/${photo.id}`)} variant='contained' color='primary'>Details</Button>
             </div>
           </div>
         )}
       </div>
+      <Stack spacing={2}>
+        <Pagination 
+        page={page} 
+        onChange={(_, num)=> setPage(num)} 
+        count={totalPage} 
+        variant="outlined" 
+        sx={{ marginY: 3, marginX: 'auto'}}
+        renderItem={
+          (item) => (
+            <PaginationItem
+              component={Link}
+              to={`/photos/?page=${item.page}`}
+              {...item}
+              />
+          )
+        }
+        color="primary" />
+      </Stack>
     </MainLayout>
   )
 }
 
 export default Photos
+
+{/* <div className={st.info}>
+<p className={st.title}>Autor: {photo.user?.name}</p>
+<p className={st.title}>Location: {photo.user?.location ? photo.user?.location : 'Unknown'}</p>
+<p className={st.title}>Likes: {photo.likes ? photo.likes : '0'}</p>
+</div> */}
--- src/store/action-creators/photoThunk.tsx
+++ src/store/action-creators/photoThunk.tsx
@@ -9,12 +9,13 @@ import { PhotoAction, PhotoActionTypes } from "../../types/photoActionTypes"
 export const fetchPhotos = (page: number = 1, per_page: number = 20) => {
     return async (dispatch: Dispatch<PhotoAction>) => {
         try {
-            dispatch({type: PhotoActionTypes.FETCH_PHOTOS})
-            const response = await API.get(`photos?page=${page}&per_page=${per_page}&client_id=${accessKey}`)
-            dispatch({type: PhotoActionTypes.FETCH_PHOTOS_SUCCESS, payload: response.data})        
+            dispatch({ type: PhotoActionTypes.FETCH_PHOTOS })
+            const response = await API
+                .get(`photos?page=${page}&per_page=${per_page}&client_id=${accessKey}`)
+            dispatch({ type: PhotoActionTypes.FETCH_PHOTOS_SUCCESS, payload: response.data })
         } catch (e) {
             dispatch({
-                type: PhotoActionTypes.FETCH_PHOTOS_FAILED, 
+                type: PhotoActionTypes.FETCH_PHOTOS_FAILED,
                 payload: 'Error'
             })
         }
@@ -24,12 +25,12 @@ export const fetchPhotos = (page: number = 1, per_page: number = 20) => {
 export const fetchPhotosDetail = (id: string) => {
     return async (dispatch: Dispatch<DetailsAction>) => {
         try {
-            dispatch({type: DetailsActionTypes.FETCH_DETAILS})
+            dispatch({ type: DetailsActionTypes.FETCH_DETAILS })
             const response = await API.get(`photos/${id}?client_id=${accessKey}`)
-            dispatch({type: DetailsActionTypes.FETCH_DETAILS_SUCCESS, payload: response.data})
+            dispatch({ type: DetailsActionTypes.FETCH_DETAILS_SUCCESS, payload: response.data })
         } catch (e) {
             dispatch({
-                type: DetailsActionTypes.FETCH_DETAILS_FAILED, 
+                type: DetailsActionTypes.FETCH_DETAILS_FAILED,
                 payload: 'Error'
             })
         }
